b593a6f7a743471bb42b9ad411a1c2df2532eebe
Zakki <zakkinadaf56@gmail.com>
2022-12-15 22:48:34 +0530

    structure

---
+++ structure.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ struct student
+ {
+     int rollno;
+     char name[25];
+     char addr[30];
+     float sgpi;
+ };
+struct student s[63];
+
+ int main
+ {
+     int a[10];
+     int *ptr;
+     ptr++;
+     a[0]=20;
+     0[a]=10;
+     printf("\n a[1]=%d",*a,sizeof(a[0]));
+ }
